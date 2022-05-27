4ab6f80bfad075cd91b7a93ae19ee80a527dad72
Naima Hassan <naima.hassan434@gmail.com>
2022-05-27 22:13:13 +0100

    Buttons-manual approach

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
 
-  return <main>
-    <section className="menu section">
-      <div className="title">
-        <h2>our menu</h2>
-        <div className="underline"></div>
-      </div>
-      <Categories/>
-      <Menu items={menuItems}/>
-    </section>
-  </main>;
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems}/>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
